586b69b2a5fb53b87f375cba5c1817d94a7b83a3
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-14 18:40:47 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести все натуральные числа от 1 до N
+
+
+void Print(int a)
+{
+    if (a == 0)
+    {
+        return;
+    }
+    Print(a-1);
+    Console.Write(a + " ");
+}
+
+int N = 5;
+Print(N);
